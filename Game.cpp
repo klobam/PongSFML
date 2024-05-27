e51feb09ca31b33a5f9d2a79768ed416a8cce996
klobam <kmerzliy2006@gmail.com>
2024-05-27 22:23:54 +0300

    Fixed bug when game didn`t stop after win

--- Game.cpp
+++ Game.cpp
@@ -25,75 +25,85 @@ void Game::run() {
     score.setCharacterSize(15);
     float time, timePlayer1, timePlayer2;
     while (window.isOpen())
-    {   
-            sf::Event event;
-            time = clock.getElapsedTime().asMicroseconds();
-            timePlayer1 = clock.getElapsedTime().asMicroseconds();
-            timePlayer2 = clock.getElapsedTime().asMicroseconds();
-            time /= 6000;
-            timePlayer1 /= 2000;
-            timePlayer2 /= 2000;
-            clock.restart();
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-                else if (event.type == sf::Event::KeyPressed && !isGameStarted)
-                    isGameStarted = true;
-                players.movePlatform(timePlayer1, timePlayer2, event);
-            }
-            window.clear();
-            if (isGameStarted) {
-                Shape.move(time);
-                players.move();
-                if (Shape.collisions(players.getGlobalBoundPlayer1())) {
-                    Shape.changeVelocityAfterCollision();
-                }
-                if (Shape.collisions(players.getGlobalBoundPlayer2())) {
-                    Shape.changeVelocityAfterCollision();
-                }
-                window.clear();
-                Shape.draw(window);
-                players.draw(window);
-                players.chekingOutOfBounds();
-                sf::Vector2f posShape = Shape.getPos();
-                if (posShape.x < 0) {
-                    scorePlayer2++;
-                    restart();
-                }
-                else if (posShape.x > 1240) {
-                    scorePlayer1++;
-                    restart();
-                }
-                score.setString(std::to_string(scorePlayer1) + ":" + std::to_string(scorePlayer2));
-                if (scorePlayer1 >= 2) {
-                    window.clear();
-                    winText.setString("Player 1 wins \n Thanks for game=)");
-                    window.draw(winText);
-                }
-                else if (scorePlayer2 >= 2) {
-                    window.clear();
-                    winText.setString("Player 2 wins \n Thanks for game=)");
-                    window.draw(winText);
+    {
+        sf::Event event;
+        time = clock.getElapsedTime().asMicroseconds();
+        timePlayer1 = clock.getElapsedTime().asMicroseconds();
+        timePlayer2 = clock.getElapsedTime().asMicroseconds();
+        time /= 6000;
+        timePlayer1 /= 2000;
+        timePlayer2 /= 2000;
+        clock.restart();
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::KeyPressed && !isGameStarted)
+                isGameStarted = true;
+            players.movePlatform(timePlayer1, timePlayer2, event);
+        }
+        window.clear();
+        if (isGameStarted) {
 
-                }
-                window.draw(score);
-                window.display();
+            Shape.move(time);
+            players.move();
+            if (Shape.collisions(players.getGlobalBoundPlayer1())) {
+                Shape.changeVelocityAfterCollision();
             }
-            else {
-                if (scorePlayer1 != 2 || scorePlayer2 != 2) {
-                    sf::Text startText("Pong is a two-dimensional sports game that simulates table tennis\nControl for 1 player:W-up your platform.S- down ypur platform\nControl for 2 player:Up -up your platform.Down -down ypur platform\nPress any key to start", fontStart, 32);
-                    startText.setPosition(300, 300);
-                    if (scorePlayer1 > 0 || scorePlayer2 > 0) {
+            if (Shape.collisions(players.getGlobalBoundPlayer2())) {
+                Shape.changeVelocityAfterCollision();
+            }
+            
+            window.clear();
+            Shape.draw(window);
+            players.draw(window);
+            players.chekingOutOfBounds();
+            
+            sf::Vector2f posShape = Shape.getPos();
+            if (posShape.x < 0) {
+                scorePlayer2++;
+                restart();
+            }
+            else if (posShape.x > 1240) {
+                scorePlayer1++;
+                restart();
+            }
+            if (scorePlayer1 == 2) {
+                window.clear();
+                winText.setString("Player 1 wins \nThanks for game=)");
+                window.draw(winText);
+
+            }
+            else if (scorePlayer2 == 2) {
+                window.clear();
+                winText.setString("Player 2 wins \nThanks for game=)");
+                window.draw(winText);
+
+            }
+            score.setString(std::to_string(scorePlayer1) + ":" + std::to_string(scorePlayer2));
+            window.draw(score);
+            window.display();
+        }
+        else {
+            if (scorePlayer1 < 2 && scorePlayer2 < 2) {
+                sf::Text startText("Pong is a two-dimensional sports game that simulates table tennis\nControl for 1 player:W-up your platform.S- down ypur platform\nControl for 2 player:Up -up your platform.Down -down ypur platform\nPress any key to start", fontStart, 32);
+                startText.setPosition(300, 300);
+                if ((scorePlayer1 != 0 || scorePlayer2 != 0)&&(scorePlayer1 != 2 || scorePlayer2 != 2)&&!isGameStarted) {
                         startText.setString("Press any key to continue");
                         startText.setPosition(500, 300);
-                    }
-
-                    window.draw(startText);
-                    window.display();
                 }
+                window.draw(startText);
+                window.display();
             }
-        }   
+            else if((scorePlayer1 == 2 || scorePlayer2 == 2) && !isGameStarted) {
+                sf::Clock timer;
+                while (timer.getElapsedTime().asSeconds() < 5) {
+
+                }
+                window.close();
+            }
+        }
+    }
 }
 void Game::restart() {
     Shape.restart();
